22a49dcf01d8123ec5adaf313598650232af2935
bustedelbow <velozity2@gmail.com>
2025-08-06 19:59:00 +0800

    Add project files.

---
+++ agapay-backend/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using agapay_backend.Data;
+using agapay_backend.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace agapay_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly agapayDbContext _context;
+
+        public UserController(agapayDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<User>>> GetUsers()
+        {
+            return Ok(await _context.Users.ToListAsync());
+        }
+    }
+}
---
+++ agapay-backend/Data/agapayDbContext.cs
@@ -0,0 +1,10 @@
+﻿using agapay_backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace agapay_backend.Data
+{
+    public class agapayDbContext(DbContextOptions<agapayDbContext> options) : DbContext (options)
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
